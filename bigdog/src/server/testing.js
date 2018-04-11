a8412075d11d7c839f6e99252b20782e11e80298
al037 <al037@bucknell.edu>
2018-04-11 11:52:39 -0400

    node.js stuff

--- bigdog/src/server/testing.js
+++ bigdog/src/server/testing.js
@@ -1,1 +1,7 @@
 const express = require('express')
+
+const app = express()
+
+app.get('/big', (req, res) => res.send('big'))
+
+app.listen(3000, () => console.log("Example app listening on port 3000"))
