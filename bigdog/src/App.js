09f6d9d119ce12874b10e28eda03618be1c92e67
al037 <al037@bucknell.edu>
2018-04-12 16:08:27 -0400

    simple button to call server added

--- bigdog/src/App.js
+++ bigdog/src/App.js
@@ -3,18 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      submitted: false
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e){
+    fetch('http://localhost:3000/')
+    .then( response => {
+      console.log(response)
+    })
+    .catch( error => console.log("ERROR", error))   
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Big Dog Bets</h1>
+
+        <div>
+          <button onClick={this.handleClick.bind(this)} type="button">Click</button>
+        </div>              
       </div>
-    );
+    )
   }
 }
 
--- bigdog/src/server/testing.js
+++ bigdog/src/server/testing.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const app = express()
 
-app.get('/big', (req, res) => res.send('big'))
+app.get('/', (req, res) => res.send(JSON.stringify({'big': 5})))
 
 app.listen(3000, () => console.log("Example app listening on port 3000"))
