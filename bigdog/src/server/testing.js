9af10ef95038cbe75a4ff91a1fd8fcda1a15b762
al037 <al037@bucknell.edu>
2018-04-16 15:32:17 -0400

    database set up

--- bigdog/src/server/testing.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-app.get('/', (req, res) => res.send(JSON.stringify({'big': 5})))
-
-app.listen(3000, () => console.log("Example app listening on port 3000"))
