114d6d6d9b5418afa5e991105dcc28a85561d77d
Stefano Cobelli <sjc032@bucknell.edu>
2018-04-26 16:53:44 -0400

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { Container } from 'reactstrap';
 import { Row } from 'reactstrap';
 import { GoogleLogin } from 'react-google-login';
 import ReactDOM from 'react-dom';
 import Timers from './timer.js';
 
-
-var SERVER_URL = "http://localhost:8000/"
+var API = "http://localhost:5000"
 
 class DisplayWinLoss extends React.Component{
-	constructor(props){
-		super(props)
-	}
 	render(){
 		return(<div className='Row'><p id='wins' className='disp text-center col-12'>Wins: {this.props.wins}</p><p id='losses' className='disp text-center col-12'>Losses: {this.props.losses}</p></div>)
 	}
@@ -31,7 +25,6 @@ class SubmitBet extends React.Component{
 		this.setState({submittedBet:this.refs.in.value})
 		this.props.onBetEntry(this.refs.in.value)
 		document.getElementById("bet").value=""
-		document.get
 
 	}
 	render(){
@@ -41,24 +34,18 @@ class SubmitBet extends React.Component{
 }
 
 class MoneyDisplay extends React.Component{
-	constructor(props){
-		super(props)
-	}
-
 	render(){
 		return (<p id='money' className=' text-center val disp'>Money Available: ${this.props.money}</p>)
 	}
 }
 
+/*
 class OddsDisplay extends React.Component{
-	constructor(props){
-		super(props)
-	}
 	render(){
 		return (<div className='col-12'><p className='prompt text-center'>{this.state.odds}</p></div>)
 	}
 }
-
+*/
 class BetSelect extends React.Component{
 	constructor(props){
 		super(props)
@@ -72,11 +59,11 @@ class BetSelect extends React.Component{
 	this.props.onSelected(event.target.value)
 
 	}
-	
+
 	betChange(event){
 	console.log(event.target.value)
 	this.props.onBetSelected(event.target.value)
-	if(event.target.value=='All breeds'){
+	if(event.target.value === 'All breeds'){
 	fetch('https://dog.ceo/api/breeds/list')
       .then(result=>result.json())
       .then(breeds=>this.setState({breeds: breeds.message.map(u=>u.substr(0,1).toUpperCase()+u.substr(1,u.length))}))
@@ -89,10 +76,10 @@ class BetSelect extends React.Component{
             <option value={b} key={b}>{b}</option>
           )})
 	console.log(this.state.options[1])
-     
+
     }})
       .catch(err=>console.log("Couldn't fetch dog breeds", err))
-	}else if (event.target.value=='Hound breeds'){
+	}else if (event.target.value === 'Hound breeds'){
 	fetch('https://dog.ceo/api/breed/hound/list')
       .then(result=>result.json())
       .then(breeds=>this.setState({breeds: breeds.message.map(u=>u.substr(0,1).toUpperCase()+u.substr(1,u.length))}))
@@ -105,13 +92,13 @@ class BetSelect extends React.Component{
             <option value={b} key={b}>{b}</option>
           )})
 	console.log(this.state.options[1])
-     
+
     }})
       .catch(err=>console.log("Couldn't fetch dog breeds", err))
-	
+
 
 	}
-	else if (event.target.value=='Spaniel breeds'){
+	else if (event.target.value === 'Spaniel breeds'){
 	fetch('https://dog.ceo/api/breed/spaniel/list')
       .then(result=>result.json())
       .then(breeds=>this.setState({breeds: breeds.message.map(u=>u.substr(0,1).toUpperCase()+u.substr(1,u.length))}))
@@ -124,13 +111,13 @@ class BetSelect extends React.Component{
             <option value={b} key={b}>{b}</option>
           )})
 	console.log(this.state.options[1])
-     
+
     }})
       .catch(err=>console.log("Couldn't fetch dog breeds", err))
-	
+
 
 	}
-	else if (event.target.value=='Bulldog breeds'){
+	else if (event.target.value === 'Bulldog breeds'){
 	fetch('https://dog.ceo/api/breed/bulldog/list')
       .then(result=>result.json())
       .then(breeds=>this.setState({breeds: breeds.message.map(u=>u.substr(0,1).toUpperCase()+u.substr(1,u.length))}))
@@ -143,10 +130,10 @@ class BetSelect extends React.Component{
             <option value={b} key={b}>{b}</option>
           )})
 	console.log(this.state.options[1])
-     
+
     }})
       .catch(err=>console.log("Couldn't fetch dog breeds", err))
-	
+
 
 	}
 	}
@@ -156,7 +143,7 @@ class BetSelect extends React.Component{
 
 }
 
-
+/*
 class BreedSelect extends React.Component{
 	constructor(props){
 		super(props)
@@ -176,7 +163,7 @@ class BreedSelect extends React.Component{
             <option value={b} key={b}>{b}</option>
           )})
 	console.log(this.state.options[1])
-     
+
     }})
       .catch(err=>console.log("Couldn't fetch dog breeds", err))
 	}
@@ -190,19 +177,20 @@ class BreedSelect extends React.Component{
 
 	}
 }
+*/
 
 class DogDisplay extends React.Component{
 	constructor(props){
-		super(props)		
+		super(props)
 		this.placeholderurl = 'http://via.placeholder.com/350x350'
-    	this.state = { loadedbreed:"", 
-    				   currmon:500, 
-    				   displayed:"Currently displayed breed: None", 
-    				   breeds:[], 
-    				   selectedBreed:"--select breed--", 
-    				   imgurl: this.placeholderurl, 
-    				   tempurl:"", 
-    				   wins:0, 
+    	this.state = { loadedbreed:"",
+    				   currmon:500,
+    				   displayed:"Currently displayed breed: None",
+    				   breeds:[],
+    				   selectedBreed:"--select breed--",
+    				   imgurl: this.placeholderurl,
+    				   tempurl:"",
+    				   wins:0,
     				   losses:0,
     				   loggedIn: false,
     				   name: "",
@@ -222,18 +210,18 @@ class DogDisplay extends React.Component{
 	componentDidMount(){
 		console.log("mounted")
 
-		const responseGoogle = (response) => {      
-		  var token = response.Zi.id_token;      
+		const responseGoogle = (response) => {
+		  var token = response.Zi.id_token;
 		  fetch("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + token )
 		  .then(jsonResponse => {
 		    var json = jsonResponse.json()
 		    return json
-		  }).then(json => {	
-		    var id = json["email"].substr(0, json["email"].indexOf('@')) //returns user id created by google      
+		  }).then(json => {
+		    var id = json["email"].substr(0, json["email"].indexOf('@')) //returns user id created by google
 		    var name = json["name"]
 		    this.handleGoogleInfomation(id, name)
 		})
-		.catch( error => console.log("ERROR", error))       
+		.catch( error => console.log("ERROR", error))
 		}
 
 		ReactDOM.render(
@@ -242,7 +230,7 @@ class DogDisplay extends React.Component{
 		    id = "Google"
 		    clientId="954169837234-2sjs0c5ie2vg5use5oaigjouudqr2k8o.apps.googleusercontent.com"
 		    buttonText="Sign in with Google"
-		    
+
 		    onSuccess={responseGoogle}
 		    onFailure={responseGoogle}
 		  />,
@@ -250,8 +238,9 @@ class DogDisplay extends React.Component{
 		);
 	}
 
-	handleGoogleInfomation(id, name){		
-		fetch("http://localhost:8000/login/"+id)
+	handleGoogleInfomation(id, name){
+		console.log(id)
+		fetch( API + "/login/"+id)
 		.then(jsonResponse => {
 			var json = jsonResponse.json()
 			return json
@@ -260,15 +249,15 @@ class DogDisplay extends React.Component{
 			var currmon = json["money"]
 			var losses = json["losses"]
 			var wins = json["wins"]
-			this.setState({currmon: currmon, 
+			this.setState({currmon: currmon,
 						   wins: wins,
 						   losses: losses,
 						   name: name,
 						   loggedIn: true,
 						   id: id
-						   })		    	    
+						   })
 		})
-		.catch( error => console.log("ERROR", error))       
+		.catch( error => console.log("ERROR", error))
 	}
 
 	httpGetAsynchronous(theUrl, callback) {
@@ -283,16 +272,16 @@ class DogDisplay extends React.Component{
 		};
 		xmlHttp.open("GET", theUrl, true); // true for asynchronous request
 		xmlHttp.send(null);
-	}	
+	}
 	handleSelectedBet(j){
 		console.log(j)
 		this.setState({type:j, selectedBreed:"--select breed--"})
 		if(j==='All breeds'){
 		this.setState({odds:"Odds of winning: 1:81 (x10 payout)"})
-		
+
 		}else if (j==='Hound breeds'){
 		this.setState({odds:"Odds of winning: 1:6 (x3 payout)"})
-		
+
 		}else if(j==='Spaniel breeds'){
 		this.setState({odds:"Odds of winning: 1:7 (x4 payout)"})
 		}else if(j==='Bulldog breeds'){
@@ -301,7 +290,7 @@ class DogDisplay extends React.Component{
 	}
 	handleSelectedChange(event){
 		console.log(event)
-		if(this.state.type=='All breeds'){
+		if(this.state.type === 'All breeds'){
 		 fetch('https://dog.ceo/api/breeds/image/random')
         .then(resp => resp.json())
         .then(jresp => {
@@ -315,11 +304,11 @@ class DogDisplay extends React.Component{
 		}else{
 		temp[4]=t2
 		}
-		
+
 
         console.log(temp)
         this.setState({loadedbreed:temp[4].substr(0,1).toUpperCase()+temp[4].substr(1,temp[4].length),tempurl:jresp.message,selectedBreed:event})
-		}		
+		}
 	})}
 	else if (this.state.type==='Hound breeds'){
 	fetch('https://dog.ceo/api/breed/hound/images/random')
@@ -335,7 +324,7 @@ class DogDisplay extends React.Component{
 		}else{
 		temp[4]=t2
 		}
-		
+
 
         console.log(temp)
         this.setState({loadedbreed:temp[4].substr(0,1).toUpperCase()+temp[4].substr(1,temp[4].length),tempurl:jresp.message,selectedBreed:event})
@@ -356,7 +345,7 @@ class DogDisplay extends React.Component{
 		}else{
 		temp[4]=t2
 		}
-		
+
 
         console.log(temp)
         this.setState({loadedbreed:temp[4].substr(0,1).toUpperCase()+temp[4].substr(1,temp[4].length),tempurl:jresp.message,selectedBreed:event})
@@ -377,7 +366,7 @@ class DogDisplay extends React.Component{
 		}else{
 		temp[4]=t2
 		}
-		
+
 
         console.log(temp)
         this.setState({loadedbreed:temp[4].substr(0,1).toUpperCase()+temp[4].substr(1,temp[4].length),tempurl:jresp.message,selectedBreed:event})
@@ -391,13 +380,13 @@ class DogDisplay extends React.Component{
 
 	changeDogPicture(){
 
-		this.setState({imgurl: 'http://via.placeholder.com/350x350', 
+		this.setState({imgurl: 'http://via.placeholder.com/350x350',
 					   submitted: false,
 					  displayed:"Currently displayed breed: None", disable:false, messg:""})
 	}
 
-	
-	
+
+
 	/*
 	* param win: boolean value that is true if the person won
 	* param amountBet: amount of money the user bet
@@ -408,13 +397,13 @@ class DogDisplay extends React.Component{
 		if(win){
 			amountBet = amountBet * multiplier
 			msg="Congratulations! You won $"+amountBet+"!"
-			
+
 		}else{
 			amountBet = amountBet * -1
 			msg="Incorrect! You lost $"+amountBet*-1+"!"
 		}
 
-		fetch(SERVER_URL+"update/"+this.state.id+"&"+amountBet)
+		fetch(API + "/update/"+this.state.id+"&"+amountBet)
         	.then(resp =>{
         		var json = resp.json()
         		return json
@@ -422,13 +411,13 @@ class DogDisplay extends React.Component{
 				var currmon = json["money"]
 				var losses = json["losses"]
 				var wins = json["wins"]
-				this.setState({currmon: currmon, 
+				this.setState({currmon: currmon,
 							   wins: wins,
 							   losses: losses,
 							   messg:msg
 							   })
-				console.log(json)        		
-        	}).catch(error => console.log("ERROR", error))		
+				console.log(json)
+        	}).catch(error => console.log("ERROR", error))
 	}
 	handleBetSubmitted(o){
 		console.log(this.state.loadedbreed)
@@ -442,7 +431,7 @@ class DogDisplay extends React.Component{
 		if(this.state.currmon<o){
 		this.setState({disable:false})
 		alert("Insufficient funds")
-		return 
+		return
 		}
 		if(this.state.selectedBreed==="" || this.state.selectedBreed==="--select breed--"){
 		this.setState({disable:false})
@@ -476,9 +465,9 @@ class DogDisplay extends React.Component{
 
 
 		this.handleSelectedChange(this.state.selectedBreed)
-		
 
-	
+
+
 }
 
 	render(){
@@ -489,16 +478,16 @@ class DogDisplay extends React.Component{
 						<div className='col-2'></div>
 						<div className="col-8 val" >
 							<h1 className="text-center heading" >Welcome to Big Dog Bets{'\n'+this.state.name}!</h1>
-						
-						
-					
+
+
+
 						<Row>
 							<div className="col-12 "><p className=" currbreed text-center">{this.state.displayed+'\n'+this.state.messg}</p></div>
-							<img className="img-responsive center-block" src={this.state.imgurl}/>
+							<img className="img-responsive center-block" src={this.state.imgurl} alt="dog"/>
 							<BetSelect onSelected={this.handleSelectedChange} onBetSelected={this.handleSelectedBet}/>
-							
-							
-							
+
+
+
 							<div className='col-12'><p className='prompt text-center mb10'>{this.state.odds}</p></div>
 							<SubmitBet disable={this.state.disable} onBetEntry={this.handleBetSubmitted}/>
 						</Row>
@@ -507,14 +496,14 @@ class DogDisplay extends React.Component{
 						<div className='bord'>
 						<div className='Row'>
 						<MoneyDisplay money={this.state.currmon}/></div>
-						
-						
+
+
 						<DisplayWinLoss wins={this.state.wins} losses={this.state.losses}/></div>
 						</div>
 						</Row>
-						
-						
-						
+
+
+
 					</Container-fluid>)
 		}else{
 			return(
@@ -522,7 +511,7 @@ class DogDisplay extends React.Component{
 						<h1 id="signInHeader">Please Sign In With Google To Begin</h1>
 						<h1 className="loginheading text-center"> Big Dog Bets</h1>
 						<h2 className="subheading text-center"> The best place to bet on random images of dogs.</h2>
-						<div id= "googleButton"></div> 
+						<div id= "googleButton"></div>
 					</div>
 				)
 		}
